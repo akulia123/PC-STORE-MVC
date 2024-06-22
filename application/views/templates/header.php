267aecdad83214fb9802ba1bbf3611d51a664730
akulia123 <mauliarochallii@gmail.com>
2024-06-22 21:33:33 +0700

    first commit

---
+++ application/views/templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PC Store</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/styles.css'); ?>">
+</head>
+<body>
+<header>
+    <div class="container">
+        <h1>PC Store</h1>
+    </div>
+</header>
+<div class="container">
